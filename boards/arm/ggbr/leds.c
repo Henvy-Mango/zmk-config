3016a1b210d9451b6dbb581fb807ebbb396003ff
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-05-22 04:05:44 +0800

    test

--- boards/arm/ggbr/leds.c
+++ boards/arm/ggbr/leds.c
@@ -35,7 +35,7 @@ static int led_layer_listener_cb(const zmk_event_t *eh) {
   // layer NUM -> led
   if (ev->layer == 2) {
     if (ev->state) {
-      for (int i = 0; i < 5; i++) {
+      while (true) {
         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
         k_sleep(K_MSEC(300));
         led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
