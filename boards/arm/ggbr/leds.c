e85e8c3e11e1b665a90781c7bab18f83ba495040
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-05-21 22:56:54 +0800

    test

--- boards/arm/ggbr/leds.c
+++ boards/arm/ggbr/leds.c
@@ -4,6 +4,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zmk/hid_indicators.h>
+#include <zmk/events/layer_state_changed.h>
 #include <zmk/events/hid_indicators_changed.h>
 
 #define LED_GPIO_NODE_ID DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds)
@@ -26,3 +27,21 @@ static int led_keylock_listener_cb(const zmk_event_t *eh) {
 
 ZMK_LISTENER(led_indicators_listener, led_keylock_listener_cb);
 ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
+
+// Layer state listener for layer NUM LED
+static int led_layer_listener_cb(const zmk_event_t *eh) {
+  const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
+
+  // layer NUM -> led
+  if (ev->layer == 2) {
+    if (ev->state) {
+      led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
+    } else {
+      led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
+    }
+  }
+  return 0;
+}
+
+ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
+ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
