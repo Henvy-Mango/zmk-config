ee6ce8d0c848b7863a95baf20d78e996df10fd4b
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-05-22 22:17:25 +0800

    fix

--- boards/arm/ggbr/leds.c
+++ boards/arm/ggbr/leds.c
@@ -29,22 +29,22 @@ ZMK_LISTENER(led_indicators_listener, led_keylock_listener_cb);
 ZMK_SUBSCRIPTION(led_indicators_listener, zmk_hid_indicators_changed);
 
 // Layer state listener for layer LED
-static int led_layer_listener_cb(const zmk_event_t *eh) {
-  const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
+// static int led_layer_listener_cb(const zmk_event_t *eh) {
+//   const struct zmk_layer_state_changed *ev = as_zmk_layer_state_changed(eh);
 
-  // layer NUM -> led blink NUM
-  if (ev->layer >= 2) {
-    if (ev->state) {
-      for (int i = 0; i < ev->layer; i++) {
-        led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
-        k_sleep(K_MSEC(300));
-        led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
-        k_sleep(K_MSEC(300));
-      }
-    }
-  }
-  return 0;
-}
+//   // layer NUM -> led blink NUM
+//   if (ev->layer >= 2) {
+//     if (ev->state) {
+//       for (int i = 0; i < ev->layer; i++) {
+//         led_on(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
+//         k_sleep(K_MSEC(300));
+//         led_off(led_dev, DT_NODE_CHILD_IDX(DT_ALIAS(led_caps)));
+//         k_sleep(K_MSEC(300));
+//       }
+//     }
+//   }
+//   return 0;
+// }
 
-ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
-ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
+// ZMK_LISTENER(layer_led_listener, led_layer_listener_cb);
+// ZMK_SUBSCRIPTION(layer_led_listener, zmk_layer_state_changed);
